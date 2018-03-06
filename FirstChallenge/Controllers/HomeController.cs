b85c2cffbeb2e759bb7911ecdfb414a318638cd9
James Knorr <jknorr@coachmeapps.com>
2018-03-05 22:13:19 -0800

    Add project files.

---
+++ FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = Models.ComicBookManager.GetComicBooks();
+
+            return View(comics);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var comics = Models.ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(p => p.ComicBookId == id);
+
+            return View(comic);
+        }
+
+    }
+}
